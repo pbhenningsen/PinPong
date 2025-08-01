917b5616026a9f6293a66e1f257d0e328406f461
Woe Industries <pbhenningsen@gmail.com>
2025-08-01 14:36:04 -0400

    Revert "Fucking around"
    This reverts commit 513b2bf0b921c4a47fc861bc67cfbb05e8168a7c.

--- pong-multiplayer-demo/logic/lobby.gd
+++ pong-multiplayer-demo/logic/lobby.gd
@@ -3,7 +3,7 @@ extends Control
 # Default game server port. Can be any number between 1024 and 49151.
 # Not present on the list of registered or common ports as of December 2022:
 # https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
-const DEFAULT_PORT = 8910# Remember, a port is a label for a process.
+const DEFAULT_PORT = 8910
 
 @onready var address = $Address
 @onready var host_button = $HostButton
@@ -20,11 +20,11 @@ var peer = null
 func _ready():
 	# Connect all the callbacks related to networking.
 
-	multiplayer.peer_connected.connect(_player_connected) #Emitted when this MultiplayerAPI's multiplayer_peer connects with a new peer. 
+	multiplayer.peer_connected.connect(_player_connected)
 	multiplayer.peer_disconnected.connect(_player_disconnected)
-	multiplayer.connected_to_server.connect(_connected_ok)# Emitted when this Multiplayer API's multiplayer_peer successfully connected to a server. Only emitted on clients. 
-	multiplayer.connection_failed.connect(_connected_fail) # Only emitted on clients. 
-	multiplayer.server_disconnected.connect(_server_disconnected) # Only emitted on clients. 
+	multiplayer.connected_to_server.connect(_connected_ok)
+	multiplayer.connection_failed.connect(_connected_fail)
+	multiplayer.server_disconnected.connect(_server_disconnected)
 
 #### Network callbacks from SceneTree ####
 
@@ -33,7 +33,7 @@ func _player_connected(_id):
 	# Someone connected, start the game!
 	var pong = load("res://pong.tscn").instantiate()
 	# Connect deferred so we can safely erase it from the callback.
-	pong.game_finished.connect(_end_game, CONNECT_DEFERRED) #Deferred connections trigger their callables on idle time (at the end of the frame), rather than instantly. 
+	pong.game_finished.connect(_end_game, CONNECT_DEFERRED)
 
 	get_tree().get_root().add_child(pong)
 	hide()
@@ -91,12 +91,12 @@ func _set_status(text, isok):
 
 func _on_host_pressed():
 	peer = ENetMultiplayerPeer.new()
-	var err = peer.create_server(DEFAULT_PORT, 1) # Maximum of 1 peer, since it's a 2-player game. Creates a server that listens to connections via port. 
+	var err = peer.create_server(DEFAULT_PORT, 1) # Maximum of 1 peer, since it's a 2-player game.
 	if err != OK:
 		# Is another server running?
 		_set_status("Can't host, address in use.",false)
 		return
-	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)# Compress Range Coder is good for small packets, but not things above 4 kb. 
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
 
 	multiplayer.set_multiplayer_peer(peer)
 	host_button.set_disabled(true)
